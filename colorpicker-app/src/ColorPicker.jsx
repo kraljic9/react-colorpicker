adf87952f02efe83e2326718da88f6a9f1327304
Pavle Kraljic <kraljicpavle9@gmail.com>
2025-12-18 21:51:05 +0100

    Added elements to componant

--- colorpicker-app/src/ColorPicker.jsx
+++ colorpicker-app/src/ColorPicker.jsx
@@ -1,3 +1,14 @@
-function ColorPicker() {}
+function ColorPicker() {
+  return (
+    <>
+      <div className="colorPicker-container">
+        <div className="colorPicker-block">
+          <p className="colorPicker-text"></p>
+        </div>
+        <input type="color" />
+      </div>
+    </>
+  );
+}
 
 export default ColorPicker;
